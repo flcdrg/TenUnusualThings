53d4e2dd38fd4529bfd23fbd94dddaa6e055360b
David Gardiner <david@gardiner.net.au>
2015-05-05 21:52:08 +0930

    Rounding

---
+++ DynamicHashes/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace DynamicHashes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic bag = new Bag();
+
+            //bag.Values["a"] = "b";
+
+            bag.a = "b";
+
+            Console.WriteLine(bag.a);
+            Console.ReadLine();
+        }
+    }
+
+    public class Bag : DynamicObject
+    {
+        private IDictionary<string, string> _values;
+
+        /*
+        private IDictionary<string, string> Values
+        {
+            get
+            {
+                return _values;
+            }
+            set
+            {
+                _values = value;
+            }
+        }
+        */
+        public Bag()
+        {
+            _values = new Dictionary<string, string>();
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            result = _values[binder.Name];
+
+            return true;
+            //return base.TryGetMember(binder, out result);
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            _values[binder.Name] = (string)value;
+            return true;
+        }
+
+        public override IEnumerable<string> GetDynamicMemberNames()
+        {
+            return _values.Keys;
+        }
+    }
+}
---
+++ Rounding/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Rounding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (decimal d = 0.1M; d < 1.0M; d += 0.1M)
+            {
+                Console.WriteLine("{0} {1}", d, Math.Round(d));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
