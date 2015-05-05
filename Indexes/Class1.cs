a2993c4a4995defbecfd4480366d4895c1ae5984
David Gardiner <david@gardiner.net.au>
2015-05-05 21:09:43 +0930

    Indexes

---
+++ Indexes/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Indexes
+{
+    public class BagOfTricks
+    {
+        private Random _random = new Random();
+
+        public string this[string key, int number]
+        {
+            get
+            {
+                return number.ToString();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bag = new BagOfTricks();
+
+            Console.WriteLine(bag["a", 50]);
+            Console.ReadLine();
+        }
+    }
+}
