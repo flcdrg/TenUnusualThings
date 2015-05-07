96228142dcfd7955fc414e4bc007402fc5722692
David Gardiner <david@gardiner.net.au>
2015-05-07 22:40:32 +0930

    CodeAware libraries

---
+++ CodeAware/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace CodeAware
+{
+    public class Class1
+    {
+        private SqlConnection _connection;
+
+        public Class1()
+        {
+            _connection = new SqlConnection();
+        }
+    }
+}
