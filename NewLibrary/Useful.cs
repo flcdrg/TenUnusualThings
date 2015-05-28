e42f1908ccb85901c484772b906d402af631fd80
David Gardiner <david@gardiner.net.au>
2015-05-28 13:47:37 +0930

    Message to show we're running in NewLibrary

--- NewLibrary/Useful.cs
+++ NewLibrary/Useful.cs
@@ -1,10 +1,13 @@
-﻿namespace OldLibrary
+﻿using System;
+
+namespace OldLibrary
 {
 #if false
     public class Useful
     {
         public int Calculation(int x, int y)
         {
+            Console.WriteLine("We're new!");
             return x + 2 * y - 3;
         }
     }
