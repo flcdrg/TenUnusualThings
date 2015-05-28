55b862382c48d3508f81dd75a11ac3b4ff2e135e
danielmarbach <daniel.marbach@particular.net>
2015-05-28 15:46:51 +0200

    The Easy Way

--- TaskPatterns/Program.cs
+++ TaskPatterns/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace TaskPatterns
@@ -21,37 +20,11 @@ namespace TaskPatterns
         }
     }
 
-    public class TimeSpanAwaiter : INotifyCompletion
-    {
-        private readonly TimeSpan _value;
-
-        public TimeSpanAwaiter(TimeSpan value)
-        {
-            _value = value;
-        }
-
-        public void OnCompleted(Action continuation)
-        {
-            Console.WriteLine("Sleeping for {0} seconds. Don't you wish you set to to something smaller?", _value.TotalSeconds);
-            Thread.Sleep(_value);
-            continuation();
-        }
-
-        public bool IsCompleted
-        {
-            get { return false; }
-        }
-
-        public void GetResult()
-        {
-        }
-    }
-
     public static class TimeSpanExtensions
     {
-        public static TimeSpanAwaiter GetAwaiter(this TimeSpan value)
+        public static TaskAwaiter GetAwaiter(this TimeSpan value)
         {
-            return new TimeSpanAwaiter(value);
+            return Task.Delay(value).GetAwaiter();
         }
     }
 }
