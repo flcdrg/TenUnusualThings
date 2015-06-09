9ddb2c667b4b91138b5499277146f64b4f3d4510
David Gardiner <david@gardiner.net.au>
2015-06-09 19:56:23 +0930

    Date Math

---
+++ DateMath/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using static System.Diagnostics.Debug;
+
+namespace DateMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new DateTime(2014, 12, 31);
+
+            WriteLine(d.ToString("d"));
+
+            Debugger.Break();
+
+            // years
+            WriteLine(d.AddYears(2).Year);
+            WriteLine(d.AddYears(1).AddYears(1).Year);
+
+            // days
+            WriteLine(d.AddDays(62).ToString("d"));
+            WriteLine(d.AddDays(31).AddDays(31).ToString("d"));
+
+            // months
+            WriteLine(d.AddMonths(1).ToString("d"));
+
+            WriteLine(d.AddMonths(2).ToString("d"));
+            WriteLine(d.AddMonths(1).AddMonths(1).ToString("d"));
+
+            WriteLine(d.AddMonths(3).ToString("d"));
+            WriteLine(d.AddMonths(1).AddMonths(1).AddMonths(1).ToString("d"));
+
+            WriteLine(d.AddMonths(4).ToString("d"));
+            WriteLine(d.AddMonths(1).AddMonths(1).AddMonths(1).AddMonths(1).ToString("d"));
+        }
+    }
+}
