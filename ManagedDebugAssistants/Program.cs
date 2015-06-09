9e1c8f7d4f8396465802798b406233f3e2082f8d
David Gardiner <david@gardiner.net.au>
2015-06-09 20:04:09 +0930

    MDA

---
+++ ManagedDebugAssistants/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace ManagedDebugAssistants
+{
+    // Run second time with StreamWriterBufferedDataLost MDA enabled
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stream = File.Create("file.txt");
+
+            var writer = new StreamWriter(stream);
+
+            writer.Write("This is some text");
+
+            stream.Close();
+
+            System.Diagnostics.Process.Start("file.txt");
+        }
+    }
+}
